9f8aac1f9c5c275fa19b740ca4476fe7bbb80264
Chris Cohen <chris.cohen@ixis.co.uk>
2014-09-08 16:50:08 +0100

    Added createNode() functionality for DrupalContentTypeRegistry.

--- src/DrupalContentTypeRegistry.php
+++ src/DrupalContentTypeRegistry.php
@@ -11,6 +11,9 @@ use Codeception\Module\Drupal\ContentTypeRegistry\ContentType;
 use Codeception\Module\Drupal\ContentTypeRegistry\Fields\Field;
 use Codeception\Module\Drupal\ContentTypeRegistry\ContentTypeRegistryStorageInterface;
 use Codeception\Module\Drupal\ContentTypeRegistry\ContentTypeRegistryYamlStorage;
+use Codeception\Util\WebInterface;
+use Codeception\Module\Drupal\Pages\AdminNodeAddPage;
+use InvalidArgumentException;
 
 /**
  * Class DrupalContentTypeRegistry
@@ -122,5 +125,42 @@ class DrupalContentTypeRegistry extends Module
         }
 
         return !is_null($this->storage->getContentType($type));
+    }
+
+    /**
+     * Create a node of the specified type.
+     *
+     * Note that this will not log the user in, so you need to make sure you are already logged in with a user with
+     * sufficient privileges to access the desired node creation page.
+     *
+     * @param WebInterface $I
+     *   A reference to the Tester object being used.
+     * @param string $type
+     *   The machine name of the content type to be created.
+     * @param array $data
+     *   If you want to provide custom data, the keys in this array should be the machine names of the fields to be
+     *   filled, and the values should be the data to be used. Any fields ommitted here will use any testData from
+     *   contentTypes.yml to obtain their values.
+     */
+    public function createNode(WebInterface $I, $type, $data)
+    {
+        // Make sure we are trying to create a valid content type.
+        if (!$this->isValidContentType($type)) {
+            throw new InvalidArgumentException('"' . $type . '" is not a valid content type');
+        }
+
+        $I->amOnPage(AdminNodeAddPage::route($type));
+
+        $contentType = $this->getContentType($type);
+
+        foreach ($contentType->getFields() as $field) {
+            if (isset($data[$field->getMachine()])) {
+                $field->fill($I, $data[$field->getMachine()]);
+            } else {
+                $field->fill($I);
+            }
+        }
+
+        $I->click($contentType->getSubmitSelector());
     }
 }
