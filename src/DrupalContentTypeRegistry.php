23d2028668208a1b599edb1ece64829927d9daf6
Andy Rigby <andy.rigby@ixis.co.uk>
2015-08-25 17:37:39 +0100

    Make grabLastCreatedNid() public so it can be overridden
    Allows other codeception modules to implement grabLastCreatedNid and change
    how the nid is obtained

--- src/DrupalContentTypeRegistry.php
+++ src/DrupalContentTypeRegistry.php
@@ -210,7 +210,7 @@ class DrupalContentTypeRegistry extends Module
             $title
         );
 
-        $nid = $this->grabLastCreatedNid($I);
+        $nid = $I->grabLastCreatedNid($I);
 
         $I->seeCreateNodeWasSuccessful($I, $msg, $nid);
 
@@ -253,7 +253,7 @@ class DrupalContentTypeRegistry extends Module
      * @return mixed
      *   $nid from from node edit tab, or null if not found.
      */
-    protected function grabLastCreatedNid($I)
+    public function grabLastCreatedNid($I)
     {
         // Grab the node id from the Edit tab once the node has been saved.
         $edit_url = $I->grabAttributeFrom(Page::$nodeEditTabLinkSelector, 'href');
